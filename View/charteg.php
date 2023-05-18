4f804dbf159c773c71125aebb28826a266b3b751
Thathsara <thathsaradissa2@gmail.com>
2023-05-18 13:09:51 +0530

    finalized
    removed unwanted files and finalized the project

--- View/charteg.php
+++
@@ -1,8 +0,0 @@
-<?php
-$videoUrls = file('video-urls.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-foreach ($videoUrls as $videoUrl) {
-    echo '<video controls>';
-    echo '<source src="' . $videoUrl . '">';
-    echo '</video>';
-}
-?>
